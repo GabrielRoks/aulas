02d55e32bc0327e170c901d3e6c07ee76a5bd2ba
Gabriel Roks <gabrielrokssz@gmail.com>
2026-06-02 18:58:48 -0300

    Update ex_015.py

--- ex_015.py
+++ ex_015.py
@@ -20,14 +20,15 @@ votos_cC = int(input('Digite a quantidade de votos do candidato C: '))
 votos_brancos = int(input('Digite a quantidade de votos em branco: '))
 votos_nulos = int(input('Digite a quantidade de votos nulo: '))
 
-total_votos = (print(f'A quantidade total de fotos é: {votos_cA + votos_cB + votos_cC + votos_brancos + votos_nulos}'))
+total_votos = votos_cA + votos_cB + votos_cC + votos_brancos + votos_nulos
+todos_votos = print(f'O total de votos foi: {total_votos}')
 
-votos_candidatos = print(f'A porcentagem de votos do candidato A foi de: {(votos_cA * 100) / int(total_votos):.2f}%\n'
-                         f'A porcentagem de votos do candiadto B foi de: {(votos_cB * 100) / int(total_votos):.2f}%\n'
-                         f'A porcentagem de votos do candiadto C foi de: {(votos_cC * 100) / int(total_votos):.2f}%')
+votos_candidatos = print(f'A porcentagem de votos do candidato A foi de: {(votos_cA * 100)/total_votos:.2f}%\n'
+                         f'A porcentagem de votos do candiadto B foi de: {(votos_cB * 100)/total_votos:.2f}%\n'
+                         f'A porcentagem de votos do candiadto C foi de: {(votos_cC * 100)/total_votos:.2f}%')
 
-porc_votos_brancos = print(f'A porcentagem de votos brancos é: {(votos_brancos * 100) / total_votos:.2f}%')
+porc_votos_brancos = print(f'A porcentagem de votos brancos é: {(votos_brancos * 100)/total_votos:.2f}%')
 
-porc_votos_nulos = float(print(f'A porcentagem de votos nulos é: {(votos_nulos * 100) / total_votos:.2f}%'))
+porc_votos_nulos = print(f'A porcentagem de votos nulos é: {(votos_nulos * 100)/total_votos:.2f}%')
 
-porc_votos_validos = print(f'A porcentagem de votos válidos é de: {((total_votos - votos_nulos) * 100) / total_votos:.2f}')
+porc_votos_validos = print(f'A quantidade de votos válidos é de: {total_votos - votos_nulos - votos_brancos} votos.')
